ce63ab40b6ee67534348fa8f32236515cb11e88c
= <obsidize@gmail.com>
2023-12-03 17:10:14 -0600

    bulldozer updates to pacman editor UI

--- addons/pacman/pacman_ui_ref_map.gd
+++ addons/pacman/pacman_ui_ref_map.gd
@@ -1,5 +1,9 @@
 class_name PacmanUIRefMap extends Control
 
 @export var initialize_view: Control
-@export var initialize_button: Button
 @export var standard_view: Control
+@export var addon_list_container: Control
+
+@export var initialize_button: Button
+@export var sync_button: Button
+@export var add_button: Button
